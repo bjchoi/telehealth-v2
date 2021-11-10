eaac12ef414c7dfc2f581ca9d939a18fc92ed335
Anton Lytunenko <antonlit@gmail.com>
2021-11-10 19:12:40 +0200

    Implement alpha draft of the video room.
    This version is a lift-and-shift version of the video conference app from telehealth v1 to the shell of telehealth v2.

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,8 +1,18 @@
 import type { AppProps } from 'next/app';
+import VideoProviderWrapper from '../components/Base/VideoProvider';
+import withVideoProvider from '../components/Base/VideoProvider';
 import '../css/global.css';
+import AppStateProvider, { useAppState } from '../state';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AppStateProvider>
+      <VideoProviderWrapper>
+        <Component {...pageProps} />
+      </VideoProviderWrapper>
+    </AppStateProvider>
+  );
 }
 
 export default MyApp;
