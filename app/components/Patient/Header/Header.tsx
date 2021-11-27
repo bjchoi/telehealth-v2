358fcf55b0fe4ead45e290cd9d7492a8bb2fa19a
Kostiantyn Smirnov <Kostyanuch87@gmail.com>
2021-11-27 10:40:40 +0200

    add luxon as date library and update layout styles (#20)

--- app/components/Patient/Header/Header.tsx
+++ app/components/Patient/Header/Header.tsx
@@ -5,7 +5,7 @@ export interface HeaderProps {
 export const Header = ({ name = 'Owl Health' }: HeaderProps) => {
   return (
     <div className="flex items-center justify-center h-16 w-full bg-white shadow-patientHeader">
-      <div className="font-bold text-2xl text-primary">{name}</div>
+      <h1 className="font-bold text-2xl text-primary">{name}</h1>
     </div>
   );
 };
